4549115b0de6ef1e75b173ace014959be074df6e
arsalandev <arsalan@codexnow.com>
2021-06-29 10:30:28 +0500

    JWT Route Protection

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Order = require('../models/order');
 const Product = require('../models/product');
+const checkAuth = require('../middleware/check-auth');
 
-router.get('/', (req, res, next) => {
+router.get('/', checkAuth, (req, res, next) => {
     Order.find()
         .select('product quantity _id')
         .populate('product','name')
@@ -35,7 +36,7 @@ router.get('/', (req, res, next) => {
     // });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
     Product.findById(req.body.productId)
     .then(product => {
         const order = new Order({
@@ -81,7 +82,7 @@ router.post('/', (req, res, next) => {
     // });
 });
 
-router.get('/:orderId', (req, res, next) => {
+router.get('/:orderId',checkAuth, (req, res, next) => {
     Order.findById(req.params.orderId)
     .populate('product','name')
     .exec()
@@ -106,7 +107,7 @@ router.get('/:orderId', (req, res, next) => {
     });
 });
 
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
     Order.remove({_id: req.params.orderId})
     .exec()
     .then(result => {
